59815ac845f92d5a9c9d4699d9ede0b8ee5a3af9
lramirezmi <nano_12_rm@hotmail.com>
2016-11-27 14:06:33 -0600

    actualización de interfaz del servidor
    solo falta integrarlo al servidor e implementarle las matrices.

---
+++ GUI/Grafo.java
@@ -0,0 +1,67 @@
+package GUI;
+
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Grafo extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnAtrs = new JButton("Atrás");
+	public static String[][] grafolist;
+	public static int[][] matrizadya;
+
+	/**
+	 * Create the frame.
+	 */
+	public Grafo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		btnAtrs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VentanaPrincipal.frame.setVisible(true);
+				dispose();
+			}
+		});
+		btnAtrs.setBounds(713, 724, 63, 20);
+		contentPane.add(btnAtrs);
+
+	}
+
+	public void paint(Graphics g) {
+		for (int i = 0; i < grafolist.length; i++) {
+			for (int j = 0; j < grafolist.length; j++) {
+				if (matrizadya[i][j] < 15) {
+					g.drawLine(Integer.parseInt(grafolist[i][1]) + 10, Integer.parseInt(grafolist[i][2]) + 10,
+							Integer.parseInt(grafolist[j][1]) + 10, Integer.parseInt(grafolist[j][2]) + 10);
+					
+					g.setColor(Color.WHITE);
+
+					g.fillOval(Integer.parseInt(grafolist[j][1]), Integer.parseInt(grafolist[j][2]), 20, 20);
+					g.setColor(Color.BLACK);
+
+					g.drawOval(Integer.parseInt(grafolist[i][1]), Integer.parseInt(grafolist[i][2]), 20, 20);
+					g.drawString("" + grafolist[i][0], Integer.parseInt(grafolist[i][1]),
+							Integer.parseInt(grafolist[i][2]));
+
+				}else{
+					g.drawOval(Integer.parseInt(grafolist[i][1]), Integer.parseInt(grafolist[i][2]), 20, 20);
+					g.drawString("" + grafolist[i][0], Integer.parseInt(grafolist[i][1]),
+							Integer.parseInt(grafolist[i][2]));
+				}
+			}
+
+		}
+	}
+
+}
---
+++ Proceso/Main.java
@@ -0,0 +1,24 @@
+package Proceso;
+
+import java.awt.EventQueue;
+
+import GUI.VentanaPrincipal;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
